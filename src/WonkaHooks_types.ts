6e1446850c219db87e239d0fd078db999c9ee281
Marcin Dziewulski <hello@mobily.pl>
2021-03-23 23:52:41 +0100

    Update TS types

--- src/WonkaHooks_types.ts
+++ src/WonkaHooks_types.ts
@@ -1,24 +1,26 @@
 import { sourceT } from 'wonka/src/Wonka_types.gen'
 
 export declare type useSource = {
-  <T>(init: () => sourceT<T>): sourceT<T>
-  <A, T>(init: (inputs: [A]) => sourceT<T>, inputs: [A]): sourceT<T>
-  <A, B, T>(init: (inputs: [A, B]) => sourceT<T>, inputs: [A, B]): sourceT<T>
-  <A, B, C, T>(init: (inputs: [A, B, C]) => sourceT<T>, inputs: [A, B, C]): sourceT<T>
-  <A, B, C, D, T>(init: (inputs: [A, B, C, D]) => sourceT<T>, inputs: [A, B, C, D]): sourceT<T>
+  <T>(initFn: () => sourceT<T>): sourceT<T>
+  <A, T>(initFn: (inputs: [A]) => sourceT<T>, inputs: [A]): sourceT<T>
+  <A, B, T>(initFn: (inputs: [A, B]) => sourceT<T>, inputs: [A, B]): sourceT<T>
+  <A, B, C, T>(initFn: (inputs: [A, B, C]) => sourceT<T>, inputs: [A, B, C]): sourceT<T>
+  <A, B, C, D, T>(initFn: (inputs: [A, B, C, D]) => sourceT<T>, inputs: [A, B, C, D]): sourceT<T>
   <A, B, C, D, E, T>(
-    init: (inputs: [A, B, C, D, E]) => sourceT<T>,
+    initFn: (inputs: [A, B, C, D, E]) => sourceT<T>,
     inputs: [A, B, C, D, E],
   ): sourceT<T>
   <A, B, C, D, E, F, T>(
-    init: (inputs: [A, B, C, D, E, F]) => sourceT<T>,
+    initFn: (inputs: [A, B, C, D, E, F]) => sourceT<T>,
     inputs: [A, B, C, D, E, F],
   ): sourceT<T>
 }
 export declare type useSourceState = {
-  <T>(input: sourceT<T>, initialState: T): T
-  <T>(input: sourceT<T>, initialState?: T): T | undefined
+  <T>(source: sourceT<T>, initialState: T): T
+  <T>(source: sourceT<T>, initialState?: T): T | undefined
 }
+export declare type useSubscription = <A>(source: sourceT<A>, nextFn?: (value: A) => void) => void
 
+export declare const useSubscription: useSubscription
 export declare const useSource: useSource
 export declare const useSourceState: useSourceState
